2c2803612b997ee15339bb4f313a148190607bb4
Amila  Maharachchi <amila@Amilas-MacBook-Pro.local>
2018-06-05 17:40:30 +0530

    changed the message

--- actions/remote.js
+++ actions/remote.js
@@ -13,7 +13,7 @@
   *
   */
 function main(params) {
-    return { gitUrl : "https://github.com/" + params.username + "/" + params.repository, manifestPath : ".", update: "Merged from merge branch" };
+    return { gitUrl : "https://github.com/" + params.username + "/" + params.repository, manifestPath : ".", update: "Merged from merge branch 5th June" };
 }
 
 exports.main = main;
